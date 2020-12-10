ed46f836e57a2c8d0e38369acc30071be65c072a
Karol Majchrzak <kmajchrzak1999@gmail.com>
2020-12-10 19:10:07 +0100

    Working with files added user menu

--- RuchyBrowna.py
+++ RuchyBrowna.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from random import randint
 
 
+# def zapisz_dane:
+
 def wykres(osOdcietych, osRzednych, wspolrzednaX, wspolrzednaY, wektorPrzesuniecia):
     poczatekUkladuX = 0
     poczatekUkladuY = 0
@@ -39,6 +41,7 @@ def ruchyBrowna():
         wspolrzednaY = wspolrzednaY + krokPrzesuniecia * np.sin(radian)
         wspolrzednaX = round(wspolrzednaX, 2)
         wspolrzednaY = round(wspolrzednaY, 2)
+        print("x:", wspolrzednaX, "y:", wspolrzednaY)
         osOdcietych.append(wspolrzednaX)
         osRzednych.append(wspolrzednaY)
 
@@ -49,4 +52,52 @@ def ruchyBrowna():
     wykres(osOdcietych, osRzednych, wspolrzednaX, wspolrzednaY, wektorPrzesuniecia)
 
 
-ruchyBrowna()
+def wczytajRuchyBrownaZPliku():
+    nazwaPliku = input("Wpisz nazwe pliku z ktorego chcesz wczytac dane: ")
+
+    osOdcietychNowa = [0]
+    osRzednychNowa = [0]
+
+    try:
+        plik = open(nazwaPliku, 'r')
+        fileLines = []
+        line = plik.readline()
+        while line != "":
+            line = line.replace("\n", "")
+            fileLines.append(line)
+            line = plik.readline()
+    except FileNotFoundError:
+        print("Plik nie istnieje")
+
+    i = 0
+    while i < 9:
+        osOdcietychNowa.append(float(fileLines[i]))
+        i = i + 1
+        osRzednychNowa.append(float(fileLines[i]))
+
+    nowaWspolrzednaX = float(fileLines[8])
+    nowaWspolrzednaY = float(fileLines[9])
+    nowyWektorPrzesuniecia = np.fabs(np.sqrt(pow(float(nowaWspolrzednaX), 2) + pow(float(nowaWspolrzednaY), 2)))
+    print("Wektor przesuniecia: {:.2f}".format(nowyWektorPrzesuniecia))
+    plik.close()
+    wykres(osOdcietychNowa, osRzednychNowa, float(nowaWspolrzednaX), float(nowaWspolrzednaY), nowyWektorPrzesuniecia)
+
+
+def menu():
+    print("Witaj w programie ktory symuluje Ruchy Browna\n\n")
+    print("-------------------MENU------------------------")
+    print("1. Symulacja Ruchow Browna")
+    print("2. Symulacja Ruchow Browna z danymi przygotowanymi w pliku")
+    print("9. Koniec Programu")
+    wybor = int(input("Wybierz opcje z menu "))
+    if (wybor == 1):
+        ruchyBrowna()
+    elif (wybor == 2):
+        wczytajRuchyBrownaZPliku()
+    elif (wybor == 9):
+        exit(0)
+    else:
+        print("Przypominam ze numer mial byc 1 2 lub 9")
+
+
+menu()
